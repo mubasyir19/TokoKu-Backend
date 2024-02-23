6a8b11de7475f3e87b080cad89124dccaf2c9b63
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-02-23 11:17:54 +0700

    add(auth): feature login & register

--- app/Customer/controller.js
+++ app/Customer/controller.js
@@ -1,13 +1,84 @@
 const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
+const { pelanggan } = require('../../db/models');
 
 module.exports = {
-  login: async (req, res) => {
+  login: async (req, res, next) => {
+    const { usernameEmail, password } = req.body;
     try {
-    } catch (error) {}
+      let user;
+      if (usernameEmail.includes('@')) {
+        user = await pelanggan.findOne({ where: { email: usernameEmail } });
+      } else {
+        user = await pelanggan.findOne({ where: { username: usernameEmail } });
+      }
+
+      const checkPassword = bcrypt.compareSync(password, user.password);
+
+      if (checkPassword) {
+        const token = jwt.sign(
+          {
+            userLogin: {
+              id: user.id,
+              email: user.email,
+              nama_pelanggan: user.nama_pelanggan,
+              alamat: user.alamat,
+              no_telepon: user.no_telepon,
+            },
+          },
+          'secret'
+        );
+        res.status(200).json({
+          message: 'Success Login',
+          data: token,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   },
-  register: async (req, res) => {
+  register: async (req, res, next) => {
+    const accountId = uuid.v4();
     try {
-    } catch (error) {}
+      const { email, username, password, nama_pelanggan, alamat, no_telepon, confirmPassword } = req.body;
+
+      const checkEmail = await pelanggan.findOne({ where: { email: email } });
+      if (checkEmail) {
+        res.status(403).json({ message: 'Email has been registered' });
+      }
+
+      const checkUsername = await pelanggan.findOne({ where: { username: username } });
+      if (checkUsername) {
+        res.status(403).json({ message: 'Username has been registered' });
+      }
+
+      if (password !== confirmPassword) {
+        res.status(403).json({ message: "Password and confirm password doesn't match" });
+      }
+
+      const accountRegister = await pelanggan.create({
+        id: accountId,
+        email,
+        username,
+        password: bcrypt.hashSync(password, 10),
+        nama_pelanggan,
+        alamat,
+        no_telepon,
+      });
+
+      delete accountRegister.dataValues.password;
+
+      res.status(201).json({
+        message: 'Success signup',
+        data: accountRegister,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   },
   forgotPassword: async (req, res) => {
     /**
